36b414674927bd35291af640ae3dd9d34ef230d8
Manjeet Yadav <Manjeet.Yadav@blackbaud.me>
2019-11-11 22:11:46 +0530

    logging and overview doc

---
+++ microservices/Logging/ConfigSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Logging
+{
+    public class ConfigSettings
+    {
+        public string message { get; set; }
+    }
+}
